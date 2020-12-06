1b1712cf6ec5119a057a44ef751784ce1a484675
Astral Gnome <reedkeenan@gmail.com>
2020-12-05 23:24:26 -0800

    Managed my expectations. Began implementing simplest working version of assignment, with an eye on improving it in the future.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-# ${data.favColor}
+## ${data.description}
+
+## ${data.tableCont}
+
+## ${data.installInst}
+
+## ${data.usageInfo}
+
+## ${data.license}
+
+## ${data.contribGuide}
+
+## ${data.testInst}
+
+## ${data.questions}
+
+## ${data.favColor}
 
 `;
 }
